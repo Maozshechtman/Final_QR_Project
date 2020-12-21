11f1504861dbcc207f7f51ec46a89a8e0c3f4912
Maoz M Shechtman <maozshechtman@gmail.com>
2020-12-21 16:11:22 +0200

    Added Some new Classes

---
+++ Final_QR_Project/BE/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public String UserName { get; set; }
+        public String PassWord { get; set; }
+        public String Email { get; set; }
+    }
+}
