5d1e18c519244ef5cc556493c6a5a8d2f4405744
Maoz M Shechtman <maozshechtman@gmail.com>
2020-12-22 11:40:02 +0200

    I Created  pdf  creator  and installed  the matiral desing  UI elemnts.I still nedd to check how to connect  the Project to my google drive   account

---
+++ Final_QR_Project/BL/PdfTest.cs
@@ -0,0 +1,28 @@
+﻿using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class PdfTest
+    {
+        
+       public static void CreatePdf()
+        {
+            PdfDocument pdf = new PdfDocument();
+            PdfPage pdfPage = pdf.AddPage();
+            XGraphics graph = XGraphics.FromPdfPage(pdfPage);
+            XFont font = new XFont("Verdana", 20, XFontStyle.Bold);
+            graph.DrawString("This is my first PDF document.Thank U !", font, XBrushes.Black,
+            new XRect(0, 0, pdfPage.Width.Point, pdfPage.Height.Point), 
+            XStringFormats.TopLeft);
+
+            pdf.Save("firstpage.pdf");
+
+        }
+    }
+}
--- Final_QR_Project/Final_QR_Project/MainWindow.xaml.cs
+++ Final_QR_Project/Final_QR_Project/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using PdfSharp;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
 
 namespace Final_QR_Project
 {
@@ -23,6 +27,19 @@ namespace Final_QR_Project
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            PdfDocument pdf = new PdfDocument();
+            pdf.Info.Title = "My First PDF";
+            PdfPage pdfPage = pdf.AddPage();
+            XGraphics graph = XGraphics.FromPdfPage(pdfPage);
+            XFont font = new XFont("Verdana", 20, XFontStyle.Bold);
+            graph.DrawString("This is my first PDF document", font, XBrushes.Black, new XRect(0, 0, pdfPage.Width.Point, pdfPage.Height.Point), XStringFormats.Center);
+            string pdfFilename = "firstpage.pdf";
+            pdf.Save(pdfFilename);
+            Process.Start(pdfFilename);
         }
     }
 }
